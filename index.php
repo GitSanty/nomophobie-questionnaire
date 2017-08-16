ad93b31b2794ec224ff6a29b13a82bfd5d7585a3
Santiago Astete <sastetec@hotmail.com>
2017-08-17 00:01:10 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,374 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     
+   
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>nomophobie</title>
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link href="css/style.css" rel="stylesheet" type="text/css" />
+
+
+    </head>
+
+<body>
+
+
+ 
+ 
+	<div class="main">
+   
+       <div class="q-one-box current">
+        <h3>Question 1</h3>
+        <p class="quest-title">Je me sentirais mal à l’aise sans un accès constant à de l’information sur mon téléphone cellulaire.</p>
+        <span id="colormsg">Fortement en désaccord</span>
+        <input type="radio"  value="1" name="q-one" />1
+         <input type="radio"  value="2" name="q-one"/>2
+         <input type="radio"  value="3" name="q-one"/>3
+         <input type="radio"  value="4" name="q-one"/>4
+         <input type="radio"  value="5" name="q-one"/>5
+         <input type="radio"  value="6" name="q-one"/>6
+         <input type="radio"  value="7" name="q-one"/>7
+         <span id="colormsg">Fortement en accord</span>
+         </br> </br>
+         <button class="answer-btn">Submit réponse</button>
+     </div>
+    
+
+    <div class="q-two-box hidden">
+        <h3>Question 2</h3>
+        <p class="quest-title">Je me sentirais agacé si je ne pouvais pas consulter de l’information sur mon téléphone quand bon me semble.</p>
+         <span id="colormsg">Fortement en désaccord</span>
+         <input type="radio"  value="1" name="q-two"/>1
+         <input type="radio"  value="2" name="q-two"/>2
+         <input type="radio"  value="3" name="q-two"/>3
+         <input type="radio"  value="4" name="q-two"/>4
+         <input type="radio"  value="5" name="q-two"/>5
+         <input type="radio"  value="6" name="q-two"/>6
+         <input type="radio"  value="7" name="q-two"/>7
+         <span id="colormsg">Fortement en accord</span>
+        </br> </br>
+      <button class="answer-btn">Submit réponse</button>
+
+    </div>
+
+
+    <div class="q-three-box hidden">
+      <h3>Question 3</h3>
+        <p class="quest-title">Je me sentirais nerveux si je ne pouvais pas m’informer (actualité, météo, etc.) sur mon téléphone.</p>
+        <span id="colormsg">Fortement en désaccord</span>
+         <input type="radio"  value="1" name="q-three"/>1
+         <input type="radio"  value="2" name="q-three"/>2
+         <input type="radio"  value="3" name="q-three"/>3
+         <input type="radio"  value="4" name="q-three"/>4
+         <input type="radio"  value="5" name="q-three"/>5
+         <input type="radio"  value="6" name="q-three"/>6
+         <input type="radio"  value="7" name="q-three"/>7
+         <span id="colormsg">Fortement en accord</span>
+          </br> </br>
+      <button class="answer-btn">Submit réponse</button>
+     
+</div>
+
+
+   <div class="q-four-box hidden">
+        <h3>Question 4</h3>
+        <p class="quest-title">Je me sentirais agacé si je ne pouvais pas utiliser mon téléphone ou ses fonctionnalités quand bon me semble.</p>
+        <span id="colormsg">Fortement en désaccord</span>
+        <input type="radio"  value="1" name="q-four"/>1
+         <input type="radio" value="2" name="q-four"/>2
+         <input type="radio" value="3" name="q-four"/>3
+         <input type="radio" value="4" name="q-four"/>4
+         <input type="radio" value="5" name="q-four"/>5
+         <input type="radio" value="6" name="q-four"/>6
+         <input type="radio" value="7" name="q-four"/>7 
+         <span id="colormsg">Fortement en accord</span></br> </br>
+      <button class="answer-btn">Submit réponse</button>
+   
+    </div>
+    
+    <div class="q-five-box hidden">
+        <h3>Question 5</h3>
+        <p class="quest-title">Manquer de batterie pour mon cellulaire me ferait peur.</p>
+        <span id="colormsg">Fortement en désaccord</span>
+        <input type="radio"  value="1" name="q-five"/>1
+         <input type="radio" value="2" name="q-five"/>2
+         <input type="radio" value="3" name="q-five"/>3
+         <input type="radio" value="4" name="q-five"/>4
+         <input type="radio" value="5" name="q-five"/>5 
+         <input type="radio" value="6" name="q-five"/>6 
+         <input type="radio" value="7" name="q-five"/>7  
+         <span id="colormsg">Fortement en accord</span> </br> </br>
+      <button class="answer-btn">Submit réponse</button>
+   
+    </div>
+    
+    <div class="q-six-box hidden">
+        <h3>Question 6</h3>
+        <p class="quest-title">Si j’atteignais la limite de mon plan de données, je paniquerais.</p>
+        <span id="colormsg">Fortement en désaccord</span>
+        <input type="radio"  value="1" name="q-six"/>1
+         <input type="radio" value="2" name="q-six"/>2
+         <input type="radio" value="3" name="q-six"/>3
+         <input type="radio" value="4" name="q-six"/>4
+         <input type="radio" value="5" name="q-six"/>5
+         <input type="radio" value="6" name="q-six"/>6
+         <input type="radio" value="7" name="q-six"/>7 
+         <span id="colormsg">Fortement en accord</span> </br> </br>
+      <button class="answer-btn">Submit réponse</button>
+   
+    </div>
+    
+    <div class="q-seven-box hidden">
+        <h3>Question 7</h3>
+        <p class="quest-title">Si je n’avais pas de signal ou de connexion wifi, je vérifierais constamment mon téléphone pour voir si le signal revenait.</p>
+        <span id="colormsg">Fortement en désaccord</span>
+        <input type="radio"  value="1" name="q-seven"/>1
+         <input type="radio" value="2" name="q-seven"/>2
+         <input type="radio" value="3" name="q-seven"/>3
+         <input type="radio" value="4" name="q-seven"/>4
+         <input type="radio" value="5" name="q-seven"/>5 
+         <input type="radio" value="6" name="q-seven"/>6 
+         <input type="radio" value="7" name="q-seven"/>7 
+         <span id="colormsg">Fortement en accord</span> </br> </br>
+      <button class="answer-btn">Submit réponse</button>
+   
+    </div>
+    
+    
+    <div class="q-eight-box hidden">
+        <h3>Question 8</h3>
+        <p class="quest-title">Si je ne pouvais pas utiliser mon téléphone, j’aurais peur de rester pris quelque part.</p>
+        <span id="colormsg">Fortement en désaccord</span>
+        <input type="radio"  value="1" name="q-eight"/>1
+         <input type="radio" value="2" name="q-eight"/>2
+         <input type="radio" value="3" name="q-eight"/>3
+         <input type="radio" value="4" name="q-eight"/>4
+         <input type="radio" value="5" name="q-eight"/>5 
+         <input type="radio" value="6" name="q-eight"/>6 
+         <input type="radio" value="7" name="q-eight"/>7  
+        <span id="colormsg">Fortement en accord</span> </br> </br>
+      <button class="answer-btn">Submit réponse</button>
+   
+    </div>
+    
+       
+       <div class="q-nine-box hidden">
+        <h3>Question 9</h3>
+        <p class="quest-title">Si je ne pouvais pas vérifier mon téléphone pendant quelque temps, je sentirais le besoin de le faire.</p>
+        <span id="colormsg">Fortement en désaccord</span>
+        <input type="radio"  value="1" name="q-nine"/>1
+         <input type="radio" value="2" name="q-nine"/>2
+         <input type="radio" value="3" name="q-nine"/>3
+         <input type="radio" value="4" name="q-nine"/>4
+         <input type="radio" value="5" name="q-nine"/>5
+         <input type="radio" value="6" name="q-nine"/>6
+         <input type="radio" value="7" name="q-nine"/>7 
+         <span id="colormsg">Fortement en accord</span> </br> </br>
+      <button class="answer-btn">Submit réponse</button>
+     </div>
+        
+    
+    <div class="q-ten-box hidden">
+        <h3>Question 10 ==> (Si je n’avais pas mon téléphone avec moi)</h3>
+        <p class="quest-title">Je serais inquiet de ne pas pouvoir communiquer avec mes amis ou ma famille instantanément.</p>
+        <span id="colormsg">Fortement en désaccord</span>
+        <input type="radio"  value="1" name="q-ten"/>1
+         <input type="radio" value="2" name="q-ten"/>2
+         <input type="radio" value="3" name="q-ten"/>3
+         <input type="radio" value="4" name="q-ten"/>4
+         <input type="radio" value="5" name="q-ten"/>5
+         <input type="radio" value="6" name="q-ten"/>6
+         <input type="radio" value="7" name="q-ten"/>7 
+         <span id="colormsg">Fortement en accord</span> </br> </br>
+      <button class="answer-btn">Submit réponse</button>
+   
+    </div>
+
+    
+    <div class="q-eleven-box hidden">
+        <h3>Question 11 ==> (Si je n’avais pas mon téléphone avec moi)</h3>
+        <p class="quest-title">Je serais inquiet de ne pas pouvoir être joint par mes amis ou ma famille.</p>
+        <span id="colormsg">Fortement en désaccord</span>
+        <input type="radio"  value="1" name="q-eleven"/>1
+         <input type="radio" value="2" name="q-eleven"/>2
+         <input type="radio" value="3" name="q-eleven"/>3
+         <input type="radio" value="4" name="q-eleven"/>4
+         <input type="radio" value="5" name="q-eleven"/>5
+         <input type="radio" value="6" name="q-eleven"/>6
+         <input type="radio" value="7" name="q-eleven"/>7 
+         <span id="colormsg">Fortement en accord</span> </br> </br>
+      <button class="answer-btn">Submit réponse</button>
+   
+    </div>
+    
+    
+    <div class="q-twelve-box hidden">
+        <h3>Question 12 ==> (Si je n’avais pas mon téléphone avec moi)</h3>
+        <p class="quest-title">Je serais inquiet de ne pas pouvoir recevoir de messages texte ni d’appels.</p>
+        <span id="colormsg">Fortement en désaccord</span>
+        <input type="radio"  value="1" name="q-twelve"/>1
+         <input type="radio" value="2" name="q-twelve"/>2
+         <input type="radio" value="3" name="q-twelve"/>3
+         <input type="radio" value="4" name="q-twelve"/>4
+         <input type="radio" value="5" name="q-twelve"/>5
+         <input type="radio" value="6" name="q-twelve"/>6
+         <input type="radio" value="7" name="q-twelve"/>7
+         <span id="colormsg">Fortement en accord</span> </br> </br>
+      <button class="answer-btn">Submit réponse</button>
+   
+    </div>
+    
+    <div class="q-thirteen-box hidden">
+        <h3>Question 13 ==> (Si je n’avais pas mon téléphone avec moi)</h3>
+        <p class="quest-title">Je me sentirais anxieux de ne pas pouvoir garder le contact avec ma famille ou mes amis.</p>
+        <span id="colormsg">Fortement en désaccord</span>
+        <input type="radio"  value="1" name="q-thirteen"/>1
+         <input type="radio" value="2" name="q-thirteen"/>2
+         <input type="radio" value="3" name="q-thirteen"/>3
+         <input type="radio" value="4" name="q-thirteen"/>4
+         <input type="radio" value="5" name="q-thirteen"/>5
+         <input type="radio" value="6" name="q-thirteen"/>6
+         <input type="radio" value="7" name="q-thirteen"/>7
+        <span id="colormsg">Fortement en accord</span> </br> </br>
+      <button class="answer-btn">Submit réponse</button>
+   
+    </div>
+    
+    
+    <div class="q-fourteen-box hidden">
+        <h3>Question 14 ==> (Si je n’avais pas mon téléphone avec moi)</h3>
+        <p class="quest-title">Je serais inquiet de ne pas savoir si quelqu’un essaie de me rejoindre.</p>
+        <span id="colormsg">Fortement en désaccord</span>
+        <input type="radio"  value="1" name="q-fourteen"/>1
+         <input type="radio" value="2" name="q-fourteen"/>2
+         <input type="radio" value="3" name="q-fourteen"/>3
+         <input type="radio" value="4" name="q-fourteen"/>4
+         <input type="radio" value="5" name="q-fourteen"/>5
+         <input type="radio" value="6" name="q-fourteen"/>6
+         <input type="radio" value="7" name="q-fourteen"/>7 
+        <span id="colormsg">Fortement en accord</span> </br> </br>
+      <button class="answer-btn">Submit réponse</button>
+   
+    </div>
+    
+    
+    <div class="q-fifteen-box hidden">
+        <h3>Question 15 ==> (Si je n’avais pas mon téléphone avec moi)</h3>
+        <p class="quest-title">Je me sentirais anxieux de savoir que ma connexion constante avec mes amis et ma famille est interrompue.</p>
+        <span id="colormsg">Fortement en désaccord</span>
+        <input type="radio"  value="1" name="q-fifteen"/>1
+         <input type="radio" value="2" name="q-fifteen"/>2
+         <input type="radio" value="3" name="q-fifteen"/>3
+         <input type="radio" value="4" name="q-fifteen"/>4
+         <input type="radio" value="5" name="q-fifteen"/>5
+         <input type="radio" value="6" name="q-fifteen"/>6
+         <input type="radio" value="7" name="q-fifteen"/>7 
+        <span id="colormsg">Fortement en accord</span> </br> </br>
+      <button class="answer-btn">Submit réponse</button>
+   
+    </div>
+    
+    
+    <div class="q-sixteen-box hidden">
+        <h3>Question 16 ==> (Si je n’avais pas mon téléphone avec moi)</h3>
+        <p class="quest-title">Je serais inquiet d’être déconnecté de mon identité en ligne.</p>
+        <span id="colormsg">Fortement en désaccord</span>
+        <input type="radio"  value="1" name="q-sixteen"/>1
+         <input type="radio" value="2" name="q-sixteen"/>2
+         <input type="radio" value="3" name="q-sixteen"/>3
+         <input type="radio" value="4" name="q-sixteen"/>4
+         <input type="radio" value="5" name="q-sixteen"/>5
+         <input type="radio" value="6" name="q-sixteen"/>6
+         <input type="radio" value="7" name="q-sixteen"/>7 
+        <span id="colormsg">Fortement en accord</span> </br> </br>
+      <button class="answer-btn">Submit réponse</button>
+   
+    </div>
+    
+    
+     <div class="q-seventeen-box hidden">
+        <h3>Question 17 ==> (Si je n’avais pas mon téléphone avec moi)</h3>
+        <p class="quest-title">Je serais mal à l’aise de ne pas pouvoir rester à jour dans l’actualité de mes réseaux sociaux.</p>
+        <span id="colormsg">Fortement en désaccord</span>
+        <input type="radio"  value="1" name="q-seventeen"/>1
+         <input type="radio" value="2" name="q-seventeen"/>2
+         <input type="radio" value="3" name="q-seventeen"/>3
+         <input type="radio" value="4" name="q-seventeen"/>4
+         <input type="radio" value="5" name="q-seventeen"/>5
+         <input type="radio" value="6" name="q-seventeen"/>6
+         <input type="radio" value="7" name="q-seventeen"/>7 
+         <span id="colormsg">Fortement en accord</span> </br> </br>
+      <button class="answer-btn">Submit réponse</button>
+   
+    </div>
+    
+    
+    <div class="q-eigthteen-box hidden">
+        <h3>Question 18 ==> (Si je n’avais pas mon téléphone avec moi)</h3>
+        <p class="quest-title">Je serais mal à l’aise de ne pas pouvoir recevoir les notifications de mes connexions et de mes réseaux en ligne.</p>
+        <span id="colormsg">Fortement en désaccord</span>
+        <input type="radio"  value="1" name="q-eigthteen"/>1
+         <input type="radio" value="2" name="q-eigthteen"/>2
+         <input type="radio" value="3" name="q-eigthteen"/>3
+         <input type="radio" value="4" name="q-eigthteen"/>4
+         <input type="radio" value="5" name="q-eigthteen"/>5
+         <input type="radio" value="6" name="q-eigthteen"/>6
+         <input type="radio" value="7" name="q-eigthteen"/>7 
+        <span id="colormsg">Fortement en accord</span> </br> </br>
+      <button class="answer-btn">Submit réponse</button>
+   
+    </div>
+    
+    
+    <div class="q-nineteen-box hidden">
+        <h3>Question 19 ==> (Si je n’avais pas mon téléphone avec moi)</h3>
+        <p class="quest-title">Je me sentirais anxieux de ne pas pouvoir vérifier ma boîte de courriels.</p>
+        <span id="colormsg">Fortement en désaccord</span>
+        <input type="radio"  value="1" name="q-nineteen"/>1
+         <input type="radio" value="2" name="q-nineteen"/>2
+         <input type="radio" value="3" name="q-nineteen"/>3
+         <input type="radio" value="4" name="q-nineteen"/>4
+         <input type="radio" value="5" name="q-nineteen"/>5
+         <input type="radio" value="6" name="q-nineteen"/>6
+         <input type="radio" value="7" name="q-nineteen"/>7 
+        <span id="colormsg">Fortement en accord</span> </br> </br>
+      <button class="answer-btn">Submit réponse</button>
+   
+    </div>
+    
+    
+    
+    <div class="q-twenty-box hidden">
+        <h3>Question 20 ==> (Si je n’avais pas mon téléphone avec moi)</h3>
+        <p class="quest-title">Je me sentirais bizarre parce que je ne saurais pas quoi faire.</p>
+        <span id="colormsg">Fortement en désaccord</span>
+        <input type="radio"  value="1" name="q-twenty"/>1
+         <input type="radio" value="2" name="q-twenty"/>2
+         <input type="radio" value="3" name="q-twenty"/>3
+         <input type="radio" value="4" name="q-twenty"/>4
+         <input type="radio" value="5" name="q-twenty"/>5
+         <input type="radio" value="6" name="q-twenty"/>6
+         <input type="radio" value="7" name="q-twenty"/>7 
+        <span id="colormsg">Fortement en accord</span> </br> </br>
+      <button class="answer-btn">Submit réponse</button>
+   
+    </div>
+
+    <div class="endbox hidden">
+      <h3>Votre score finale est <span class="final-score">2</span>!</h3>
+      <h3>Plus votre résultat est élevé, plus le niveau de votre nomophobie est élevé (20 étant le score le moins élevé et 140 étant le score le plus élevé).</h3>
+      <button class="reset" type="reset">Play Again?</button>
+    </div>
+    </div>
+ 
+<script src="js/jquery.min.js"></script>
+<script src="js/app.js"></script>
+
+</body>
+
+
+    </html>
+
+
+ 
